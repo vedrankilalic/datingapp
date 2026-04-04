92e7f4f8ffe75dcaae40fd9e0772c888df990099
vedrankilalic <vedrankilalic@gmail.com>
2026-04-04 19:25:42 +0200

    Added angular components

---
+++ client/src/features/account/register/register.ts
@@ -0,0 +1,32 @@
+import { Component, inject, Inject, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RegisterCreds, User } from '../../../types/user';
+import { AccountService } from '../../../core/services/account-service';
+import { errorContext } from 'rxjs/internal/util/errorContext';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  private accountService = inject(AccountService)
+  cancelRegister = output<boolean>();
+  protected creds = {} as RegisterCreds;
+
+  register() {
+    this.accountService.register(this.creds).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
+    
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
