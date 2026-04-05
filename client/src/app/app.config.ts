77aa017bdc74b57b720b99e77e4db56a59a2bb5b
vedrankilalic <vedrankilalic@gmail.com>
2026-04-06 00:50:25 +0200

    Added Routing

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,14 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { InitService } from '../core/services/init-service';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes, withViewTransitions()),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+
+      return new Promise<void>((resolve) => {
+        setTimeout(async() => {
+          try{
+        return lastValueFrom(initService.init());
+      } finally {
+        const splash = document.getElementById('initial-splash');
+        if(splash){
+          splash.remove();
+        }
+        resolve()
+      }
+        }, 500)
+      })
+      
+    })
   ]
 };
