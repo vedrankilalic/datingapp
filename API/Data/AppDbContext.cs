c2c26b9ab931b09fc442351d7856dc128876aecf
vedrankilalic <vedrankilalic@gmail.com>
2026-04-21 18:58:35 +0200

    Add Repository

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -8,4 +8,6 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     
     public DbSet<AppUser> Users { get; set; }
+    public DbSet<Member> Members {get; set;}
+    public DbSet<Photo> Photos {get; set;}
 }
